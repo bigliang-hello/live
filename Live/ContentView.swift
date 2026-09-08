12c213b289928d9ff70602ffc5332ca3264e8b17
xieliang <xieliang@bopei.com.cn>
2026-09-08 15:12:04 +0800

    Initial Commit

---
+++ Live/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Live
+//
+//  Created by bigliang on 2026/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Live/LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveApp.swift
+//  Live
+//
+//  Created by bigliang on 2026/9/8.
+//
+
+import SwiftUI
+
+@main
+struct LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
